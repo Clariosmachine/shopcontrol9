29b163d6db99049c0a66bb627a771424dbef8207
João Rocha <65980550+Clariosmachine@users.noreply.github.com>
2021-11-27 12:22:41 -0300

    Corrige códigos dos produtos que estão com mais de 6 dígitos

---
+++ Alterar código dos produtos quando cliente bipa EAN no campo código.sql
@@ -0,0 +1,5 @@
+select ordem, codigo, nome from prod_serv where len(codigo) >6
+
+update prod_serv set codigo = ordem - 6819 where len(codigo) >6
+
+-- substituir "- 6819" algum valor que queira que o código pegue ou retirar para ficar igual à ordem.
