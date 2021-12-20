9aff61a7d032d670bb554d2cff80df1842b5cfb0
João Rocha <65980550+Clariosmachine@users.noreply.github.com>
2021-12-20 17:53:09 -0300

    Update de movimento_prod_serv baseado na movimento
    Atualizar a data de efetivação do estoque na tabela movimento_prod_serv baseando-se na data de efetivação do estoque da tabela movimento.
    Usada quando a data de efetivação do estoque na tabela movimento é alterada via banco.

---
+++ Update de movimento_prod_serv baseado na movimento.sql
@@ -0,0 +1,14 @@
+update Movimento_Prod_Serv
+set Data_Efetivacao_Estoque = movimento.Data_Efetivado_Financeiro
+-- Origem do dado
+from movimento
+inner join
+Movimento_Prod_Serv
+on
+Movimento_prod_serv.ordem_movimento = movimento.ordem 
+and Movimento.Tipo_Operacao = 'VND'
+-- condição
+where 
+Movimento_Prod_Serv.Data_Efetivacao_Estoque is not null
+and
+Movimento_Prod_Serv.ordem > 123
