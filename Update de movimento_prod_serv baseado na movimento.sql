0112e495f9da7bf49b76dab47c6b72bdbefab5c0
João Rocha <65980550+Clariosmachine@users.noreply.github.com>
2021-12-22 11:32:15 -0300

    Upgrade do comando
    Agora o comando atualiza a tabela Movimento_Prod_Serv e a tabela Movimento também. Assim a data de efetivação do estoque fica igual a data de efetivação do financeiro nos produtos e na sequência.

--- Update de movimento_prod_serv baseado na movimento.sql
+++ Update de movimento_prod_serv baseado na movimento.sql
@@ -1,14 +1,33 @@
+-- Criacao da tabela temporaria com os numeros das sequencias
+create table
+#sequencias (sequencia int)
+
+-- Popular a tabela temporaria com os numeros das sequencias desejadas
+-- Informar cada sequencia entre parenteses e colocar a virgula fora, exceto na ultima porque nao tem a virgula
+-- Retire esses valores de exemplo abaixo
+insert into #sequencias values
+	(12825),
+	(12829),
+	(12830)
+
+
+-- Update da tabela Movimento_Prod_Serv pra corrigir data de efetivacao do estoque de cada produto que faz parte das sequencias indicadas acima
 update Movimento_Prod_Serv
-set Data_Passou_Efetivacao_Estoque = movimento.Data_Efetivado_Financeiro
--- Origem do dado
+set Data_Efetivacao_Estoque = movimento.Data_Efetivado_Financeiro, Data_Passou_Efetivacao_Estoque = movimento.Data_Efetivado_Financeiro
 from movimento
 inner join
 Movimento_Prod_Serv
 on
 Movimento_prod_serv.ordem_movimento = movimento.ordem 
 and Movimento.Tipo_Operacao = 'VND'
--- condição
+and movimento.Sequencia in (select sequencia from #sequencias)
+-- condicao que informa que isso so ira ocorrer em sequencias que tiveram o estoque efetivado
 where 
 Movimento_Prod_Serv.Data_Efetivacao_Estoque is not null
-and
-Movimento_Prod_Serv.ordem > 123
+
+-- update da tabela Movimento pra corrigir a data de efetivacao do estoque na sequencia
+update Movimento
+set Data_Efetivado_Estoque = Data_Efetivado_Financeiro	
+where Movimento.Data_Efetivado_Estoque is not null
+and Movimento.Tipo_Operacao = 'VND'
+and Movimento.Sequencia in (select sequencia from #sequencias)
