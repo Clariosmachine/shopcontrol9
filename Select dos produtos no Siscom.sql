6d8598f6f54576a5438600b85d842d3e86b6a480
João Rocha <65980550+Clariosmachine@users.noreply.github.com>
2022-01-03 22:29:15 -0300

    Correção de identação

--- Select dos produtos no Siscom.sql
+++ Select dos produtos no Siscom.sql
@@ -2,18 +2,21 @@ select
     prod.codigo,
     prod.codbarra,
     prod.produto,
+    
     case
-    when
-    grupo.codigo is not null
-    then grupo.codigo
-    else 9999
+        when
+        grupo.codigo is not null
+        then grupo.codigo
+        else 9999
     end cod_grupo,
+    
     case
-    when
-    grupo.grupo is not null
-    then grupo.grupo
-    else 'outros'
+        when
+        grupo.grupo is not null
+        then grupo.grupo
+        else 'outros'
     end grupo,
+    
     round (prod.precovenda, 2) P_Venda, -- Arredondamento para duas casas decimais
     round (prod.precocusto, 2) P_Custo,
     est.estoque_atual,
